83c1b0e66d8558fe5c65cdba6ead3a3365f4a517
ChrisUnsworth <ChrisUnsworth@users.noreply.github.com>
2021-11-08 10:25:06 +0000

    Added AllDiffSameIntDomain Met

--- CSPSolverTests/Logic/MetTests.cs
+++ CSPSolverTests/Logic/MetTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CSPSolver.common;
 using CSPSolver.common.variables;
+using CSPSolver.Constraint.AllDiff;
 using CSPSolver.Constraint.Equal;
 using CSPSolver.Constraint.Inequality;
 using CSPSolver.Model;
@@ -174,6 +175,45 @@ namespace CSPSolverTests.Logic
 
             v3.SetMax(state, 2);
             v4.SetMin(state, 3);
+
+            Assert.IsFalse(con2.CanBeMet(state));
+            Assert.IsFalse(con2.IsMet(state));
+        }
+
+        [TestMethod]
+        public void AllDiffSameIntDomainMetTest()
+        {
+            var sb = new StateBuilder();
+
+            var v1 = GetIntVar(1, 10, sb);
+            var v2 = GetIntVar(1, 10, sb);
+            var v3 = GetIntVar(1, 10, sb);
+            var v4 = GetIntVar(1, 10, sb);
+            var v5 = GetIntVar(1, 10, sb);
+            var v6 = GetIntVar(1, 10, sb);
+
+            var con1 = new AllDiffSameIntDomain(new ISmallIntDomainVar[] { v1, v2, v3 });
+            var con2 = new AllDiffSameIntDomain(new ISmallIntDomainVar[] { v4, v5, v6 });
+
+            var state = Initialise(sb, v1, v2, v3, v4, v5, v6);
+
+            Assert.IsTrue(con1.CanBeMet(state));
+            Assert.IsFalse(con1.IsMet(state));
+
+            v1.SetMax(state, 3);
+            v2.SetMin(state, 4);
+            v2.SetMax(state, 6);
+            v3.SetMin(state, 7);
+
+            Assert.IsTrue(con1.CanBeMet(state));
+            Assert.IsTrue(con1.IsMet(state));
+
+            Assert.IsTrue(con2.CanBeMet(state));
+            Assert.IsFalse(con2.IsMet(state));
+
+            v4.SetMax(state, 2);
+            v5.SetMax(state, 1);
+            v6.SetMax(state, 2);
 
             Assert.IsFalse(con2.CanBeMet(state));
             Assert.IsFalse(con2.IsMet(state));
