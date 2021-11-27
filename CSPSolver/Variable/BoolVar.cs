91aa0e7d05be19d6d0a3ad305338075a1ae6ecf6
ChrisUnsworth <ChrisUnsworth@users.noreply.github.com>
2021-11-27 15:34:43 +0000

    Added Bool varable

---
+++ CSPSolver/Variable/BoolVar.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CSPSolver.common;
+using CSPSolver.common.variables;
+
+namespace CSPSolver.Variable
+{
+    public readonly struct BoolVar: IBoolVar
+    {
+        public IStateRef StateRef { get; }
+
+        public int Min => 0;
+
+        public int Size => 2;
+
+        public int Max => 1;
+
+        public BoolVar(IStateRef stateRef) => StateRef = stateRef;
+
+        public bool IsTrue(IState state) => GetDomainMin(state) == 1;
+
+        public bool CanBeTrue(IState state) => GetDomainMax(state) == 1;
+
+        public bool IsFalse(IState state) => GetDomainMax(state) == 0;
+
+        public bool CanBeFalse(IState state) => GetDomainMin(state) == 0;
+
+        public (uint domain, int min, int size) GetDomain(IState state) => (state.GetDomain(StateRef, Size), Min, Size);
+
+        public bool SetDomain(IState state, uint domain)
+        {
+            var oldDom = state.GetDomain(StateRef, Size);
+            domain &= oldDom;
+            if (domain != oldDom)
+            {
+                state.SetDomain(StateRef, Size, domain);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool DomainMinus(IState state, uint domain) => SetDomain(state, state.GetDomain(StateRef, Size) & ~domain);
+
+        public IEnumerable<int> EnumerateDomain(IState state)
+        {
+            if (CanBeFalse(state)) yield return 0;
+            if (CanBeTrue(state)) yield return 1;
+        }
+
+        public int GetDomainMax(IState state) => state.GetDomainMax(StateRef, Size);
+
+        public int GetDomainMin(IState state) => state.GetDomainMin(StateRef, Size);
+
+        public bool SetMax(IState state, int max)
+        {
+            if (max >= 1) return false;
+            if (max < 0) return SetDomain(state, 0);
+
+            var newDom = state.GetDomain(StateRef, Size) & 1;
+            return SetDomain(state, newDom);
+        }
+
+        public bool SetMin(IState state, int min)
+        {
+            if (min <= 0) return false;
+            if (min > 1) return SetDomain(state, 0);
+
+            var newDom = state.GetDomain(StateRef, Size) & 2;
+            return SetDomain(state, newDom);
+        }
+
+        public bool TryGetValue(IState state, out int value)
+        {
+            value = GetDomainMin(state);
+            return value == GetDomainMax(state);
+        }
+
+        public void Initialise(IState state) => state.SetDomain(StateRef, Size, 3);
+
+        public bool IsInstantiated(IState state) => IsTrue(state) || IsFalse(state);
+
+        public bool IsEmpty(IState state) => state.GetDomain(StateRef, Size) == 0;
+
+        public Type VariableType() => typeof(bool);
+
+        public bool SetValue(IState state, object value) =>
+            Convert.ToInt32(value) switch
+            {
+                0 => SetDomain(state, 1),
+                1 => SetDomain(state, 2),
+                _ => SetDomain(state, 0),
+            };
+
+        public bool RemoveValue(IState state, object value) => 
+            Convert.ToInt32(value) switch
+            {
+                0 => SetMin(state, 1),
+                1 => SetMax(state, 0),
+                _ => false,
+            };
+
+        public string PrettyDomain(IState state) => $"{{ {string.Join(", ", EnumerateDomain(state))} }}";
+    }
+}
---
+++ CSPSolverTests/Variables/BoolVarTests.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CSPSolver.Variable;
+using CSPSolver.common;
+using CSPSolver.State;
+
+namespace CSPSolverTests.Variables
+{
+    [TestClass]
+    public class BoolVarTests
+    {
+        private (IState state, BoolVar variable) GetVar()
+        {
+            var sb = new StateBuilder();
+            var stateRef = sb.AddDomain(2);
+            var variable = new BoolVar(stateRef);
+            var state = sb.GetState();
+            variable.Initialise(state);
+            return (state, variable);
+        }
+
+        [TestMethod]
+        public void GetInitialiseTest()
+        {
+            var (s, v) = GetVar();
+            Assert.AreEqual(0, v.GetDomainMin(s));
+            Assert.AreEqual(1, v.GetDomainMax(s));
+        }
+
+        [TestMethod]
+        public void IsTrueTest()
+        {
+            var (s, v) = GetVar();
+
+            Assert.IsFalse(v.IsTrue(s));
+
+            v.SetValue(s, true);
+
+            Assert.IsTrue(v.IsTrue(s));
+
+            (s, v) = GetVar();
+
+            Assert.IsFalse(v.IsTrue(s));
+
+            v.SetValue(s, false);
+
+            Assert.IsFalse(v.IsTrue(s));
+
+            v.SetValue(s, true);
+
+            Assert.IsFalse(v.IsTrue(s));
+        }
+
+        [TestMethod]
+        public void CanBeTrueTest()
+        {
+            var (s, v) = GetVar();
+
+            Assert.IsTrue(v.CanBeTrue(s));
+
+            v.SetValue(s, true);
+
+            Assert.IsTrue(v.CanBeTrue(s));
+
+            (s, v) = GetVar();
+
+            Assert.IsTrue(v.CanBeTrue(s));
+
+            v.SetValue(s, false);
+
+            Assert.IsFalse(v.CanBeTrue(s));
+
+            v.SetValue(s, true);
+
+            Assert.IsFalse(v.CanBeTrue(s));
+        }
+
+        [TestMethod]
+        public void IsFalseTest()
+        {
+            var (s, v) = GetVar();
+
+            Assert.IsFalse(v.IsFalse(s));
+
+            v.SetValue(s, false);
+
+            Assert.IsTrue(v.IsFalse(s));
+
+            (s, v) = GetVar();
+
+            Assert.IsFalse(v.IsFalse(s));
+
+            v.SetValue(s, true);
+
+            Assert.IsFalse(v.IsFalse(s));
+
+            v.SetValue(s, false);
+
+            Assert.IsFalse(v.IsFalse(s));
+        }
+
+        [TestMethod]
+        public void CanBeFalseTest()
+        {
+            var (s, v) = GetVar();
+
+            Assert.IsTrue(v.CanBeFalse(s));
+
+            v.SetValue(s, false);
+
+            Assert.IsTrue(v.CanBeFalse(s));
+
+            (s, v) = GetVar();
+
+            Assert.IsTrue(v.CanBeFalse(s));
+
+            v.SetValue(s, true);
+
+            Assert.IsFalse(v.CanBeFalse(s));
+
+            v.SetValue(s, false);
+
+            Assert.IsFalse(v.CanBeFalse(s));
+        }
+
+        [TestMethod]
+        public void TryGetValueTest()
+        {
+            var (s, v) = GetVar();
+
+            Assert.IsFalse(v.TryGetValue(s, out _));
+
+            v.SetValue(s, true);
+
+            Assert.IsTrue(v.TryGetValue(s, out int t) && t == 1);
+
+            v.SetValue(s, false);
+
+            Assert.IsFalse(v.TryGetValue(s, out _));
+
+            (s, v) = GetVar();
+
+            v.SetValue(s, false);
+
+            Assert.IsTrue(v.TryGetValue(s, out int f) && f == 0);
+        }
+    }
+}
