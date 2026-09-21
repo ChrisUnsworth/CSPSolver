6e1dd3e242868086313b144cd4353bdfb6cda79b
ChrisUnsworth <ChrisUnsworth@users.noreply.github.com>
2026-09-21 15:52:58 +0100

    Modernise BoolConstraint/And/XOr with primary constructors
    Primary constructors and collection expressions in place of explicit
    fields/constructors and Enumerable.Empty<T>(), same style already
    used elsewhere in the codebase.

--- CSPSolver/Constraint/Bool/BoolConstraint.cs
+++ CSPSolver/Constraint/Bool/BoolConstraint.cs
@@ -3,28 +3,19 @@
 using CSPSolver.common;
 using CSPSolver.common.variables;
 
-namespace CSPSolver.Constraint.Bool
+namespace CSPSolver.Constraint.Bool;
+
+public readonly struct BoolConstraint(IBoolVar var) : IConstraint
 {
-    public readonly struct BoolConstraint : IConstraint
-    {
-        private readonly IBoolVar _var;
+    public IEnumerable<IVariable> Variables => [ var ];
 
-        public IEnumerable<IVariable> Variables => new[] { _var };
+    public bool CanBeMet(IState state) => var.CanBeTrue(state);
 
-        public BoolConstraint(IBoolVar var) => _var = var;
+    public bool IsMet(IState state) => var.IsTrue(state);
 
-        public bool CanBeMet(IState state) => _var.CanBeTrue(state);
+    public IEnumerable<IVariable> Propagate(IState state) =>
+        var.SetValue(state, true) ? [var] : [];
 
-        public bool IsMet(IState state) => _var.IsTrue(state);
-
-        public IEnumerable<IVariable> Propagate(IState state)
-        {
-            if (_var.SetValue(state, true)) yield return _var;
-        }
-
-        public IEnumerable<IVariable> NegativePropagate(IState state)
-        {
-            if (_var.SetValue(state, false)) yield return _var;
-        }
-    }
+    public IEnumerable<IVariable> NegativePropagate(IState state) =>
+        var.SetValue(state, false) ? [var] : [];
 }
--- CSPSolver/Constraint/Logic/And.cs
+++ CSPSolver/Constraint/Logic/And.cs
@@ -3,32 +3,22 @@ using System.Linq;
 
 using CSPSolver.common;
 
-namespace CSPSolver.Constraint.Logic
+namespace CSPSolver.Constraint.Logic;
+
+public readonly struct And(IConstraint con1, IConstraint con2) : IConstraint
 {
-    public readonly struct And : IConstraint
-    {
-        private readonly IConstraint _con1;
-        private readonly IConstraint _con2;
+    public IEnumerable<IVariable> Variables => con1.Variables.Concat(con2.Variables);
 
-        public And(IConstraint con1, IConstraint con2)
-        {
-            _con1 = con1;
-            _con2 = con2;
-        }
+    public bool CanBeMet(IState state) => con1.CanBeMet(state) && con2.CanBeMet(state);
 
-        public IEnumerable<IVariable> Variables => _con1.Variables.Concat(_con2.Variables);
+    public bool IsMet(IState state) => con1.IsMet(state) && con2.IsMet(state);
 
-        public bool CanBeMet(IState state) => _con1.CanBeMet(state) && _con2.CanBeMet(state);
+    public IEnumerable<IVariable> NegativePropagate(IState state) =>
+        con1.IsMet(state)
+            ? con2.NegativePropagate(state)
+            : con2.IsMet(state)
+                ? con1.NegativePropagate(state)
+                : [];
 
-        public bool IsMet(IState state) => _con1.IsMet(state) && _con2.IsMet(state);
-
-        public IEnumerable<IVariable> NegativePropagate(IState state)
-        {
-            if (_con1.IsMet(state)) return _con2.NegativePropagate(state);
-            if (_con2.IsMet(state)) return _con1.NegativePropagate(state);
-            return Enumerable.Empty<IVariable>();
-        }
-
-        public IEnumerable<IVariable> Propagate(IState state) => _con1.Propagate(state).Concat(_con2.Propagate(state));
-    }
+    public IEnumerable<IVariable> Propagate(IState state) => con1.Propagate(state).Concat(con2.Propagate(state));
 }
